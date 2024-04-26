3994d41ae415cf9cae26f0466bc9e5cbafe9e797
allzeroyou <ekdudd119@naver.com>
2024-04-26 10:58:48 +0900

    0426 intellij git test

---
+++ src/test/java/com/hana/git_test/GitTestApplicationTests.java
@@ -0,0 +1,12 @@
+package com.hana.git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
